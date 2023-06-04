1d65208e150d1a4376a18b90ffa206493955f0c9
Sally Lin <LS0723000@gmail.com>
2023-06-05 02:18:04 +0800

    implement Background class

--- main.py
+++ main.py
@@ -4,21 +4,49 @@ import os
 from audio import *
 
 
+class Background:
+    def __init__(self, path="./images/background"):
+        self._path = None
+        self._bgList = None
+        self._bg_len = None
+        self.update_background(path)
+
+    @property
+    def bg_len(self):
+        return self._bg_len
+
+    def update_background(self, path):
+        self._path = path
+        self._bgList = list(filter(lambda x: 'png' in x, os.listdir(self._path)))
+        self._bgList.sort(key=lambda x: (len(x), x))
+        self._bg_len = len(self._bgList)
+
+    def load_bg_img(self, screen, index):
+        bg_img = pygame.image.load("images/background/" + self._bgList[index])
+        bg_img = pygame.transform.scale(bg_img, (WIDTH, HEIGHT))
+
+        bg_filter = screen.copy().convert()
+        pygame.draw.rect(bg_filter, (0, 0, 0), (0, 0, WIDTH, HEIGHT))
+        bg_filter.set_alpha(100)
+
+        screen.blit(bg_img, (0, 0))
+        screen.blit(bg_filter, (0, 0))
+
+
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('Subtitle')
         self.clock = pygame.time.Clock()
-        self.music = Music()
-        self.bgList = list(filter(lambda x: 'png' in x, os.listdir('./images/background')))
+        self.music = Audio()
+        self.background = Background()
 
     def run(self):
         t = pygame.time.get_ticks()
         get_ticks_last_frame = t
 
         index = 0
-        self.bgList.sort(key=lambda x: (len(x), x))
         self.music.change_song("musics/music.mp3")
         self.music.music_start()
 
@@ -32,16 +60,9 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-            bg_img = pygame.image.load("images/background/" + self.bgList[index])
-            bg_img = pygame.transform.scale(bg_img, (WIDTH, HEIGHT))
-            index = (index + 1) % len(self.bgList)
+            self.background.load_bg_img(self.screen, index)
+            index = (index + 1) % self.background.bg_len
 
-            bg_filter = self.screen.copy().convert()
-            pygame.draw.rect(bg_filter, (0, 0, 0), (0, 0, WIDTH, HEIGHT))
-            bg_filter.set_alpha(100)
-
-            self.screen.blit(bg_img, (0, 0))
-            self.screen.blit(bg_filter, (0, 0))
             self.music.update_bars(self.screen, delta_time)
 
             pygame.display.update()
